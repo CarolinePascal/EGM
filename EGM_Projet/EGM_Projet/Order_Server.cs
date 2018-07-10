ccb1416d8978095915c815bdea38b5675f2534d5
CarolinePascal <caroline8.pascal@gmail.com>
2018-07-10 15:26:47 +0100

    Order_Server class - class for the path creation

---
+++ EGM_Projet/EGM_Projet/Order_Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGM_Projet
+{
+    public class Order_Server : Input_Server
+    {
+        //Ordered position
+        public float x;
+        public float y;
+        public float z;
+
+        public Order_Server(int IPport) : base(IPport)
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public override void Parsing(string returnData)
+        {
+            returnData = returnData.Replace('.', ',');
+            String[] substrings = returnData.Split(' ');
+
+            x = float.Parse(substrings[0]);
+            y = float.Parse(substrings[1]);
+            z = float.Parse(substrings[2]);
+        }
+
+
+    }
+}
