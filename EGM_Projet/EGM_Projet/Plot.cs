a6c6b1f6c2927456a4e7460d48381b647b15b02b
CarolinePascal <caroline8.pascal@gmail.com>
2018-07-12 13:56:35 +0100

    Fix syntax issues

--- EGM_Projet/EGM_Projet/Plot.cs
+++ EGM_Projet/EGM_Projet/Plot.cs
@@ -13,44 +13,44 @@ namespace EGM_Projet
         /// <summary>
         /// .py file path to write in
         /// </summary>
-        private string filePath = "C:/Users/carol/Desktop/Stage_1/plot.py";
+        private string _filePath = "C:/Users/carol/Desktop/Stage_1/plot.py";
 
         /// <summary>
         /// python.exe file path
         /// </summary>
-        private string python = "C:/Users/carol/appdata/local/programs/python/python36-32/python.exe";
+        private string _pythonPath = "C:/Users/carol/appdata/local/programs/python/python36-32/python.exe";
 
-        private StringBuilder text;
+        private StringBuilder _text;
 
         /// <summary>
-        /// Plot class constructor - Creates the StringBuilder text and initializes the header of the python script
+        /// Plot class constructor - Creates the StringBuilder _text and initializes the header of the python script
         /// </summary>
         public Plot()
         {
-            text = new StringBuilder();
+            _text = new StringBuilder();
 
-            text.AppendLine("#Plot Python ");
-            text.AppendLine("import matplotlib.pyplot as plt");
-            text.AppendLine("from mpl_toolkits.mplot3d import Axes3D");
+            _text.AppendLine("#Plot Python ");
+            _text.AppendLine("import matplotlib.pyplot as plt");
+            _text.AppendLine("from mpl_toolkits.mplot3d import Axes3D");
 
-            text.AppendLine(" ");
-            text.AppendLine("X=[]");
-            text.AppendLine("Y=[]");
-            text.AppendLine("Z=[]");
-            text.AppendLine("TEGM=[]");
-            text.AppendLine(" ");
-            text.AppendLine("T1=[]");
-            text.AppendLine("T2=[]");
-            text.AppendLine("T3=[]");
-            text.AppendLine("T4=[]");
-            text.AppendLine("T5=[]");
-            text.AppendLine("T6=[]");
-            text.AppendLine("T=[]");
-            text.AppendLine(" ");
+            _text.AppendLine(" ");
+            _text.AppendLine("X=[]");
+            _text.AppendLine("Y=[]");
+            _text.AppendLine("Z=[]");
+            _text.AppendLine("TEGM=[]");
+            _text.AppendLine(" ");
+            _text.AppendLine("T1=[]");
+            _text.AppendLine("T2=[]");
+            _text.AppendLine("T3=[]");
+            _text.AppendLine("T4=[]");
+            _text.AppendLine("T5=[]");
+            _text.AppendLine("T6=[]");
+            _text.AppendLine("T=[]");
+            _text.AppendLine(" ");
         }
 
         /// <summary>
-        /// Writes the recorded positions values in text
+        /// Writes the recorded positions values in _text
         /// </summary>
         /// <param name="x">x position</param>
         /// <param name="y">y position</param>
@@ -59,17 +59,17 @@ namespace EGM_Projet
         public void Fill(string x, string y, string z, string t)
         {
             var newLine = string.Format("X.append({0})", x);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("Y.append({0})", y);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("Z.append({0})", z);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("TEGM.append({0})", t);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
         }
 
         /// <summary>
-        /// Writes the recorded torques values in text
+        /// Writes the recorded torques values in _text
         /// </summary>
         /// <param name="t1">axis 1 torque</param>
         /// <param name="t2">axis 2 torque</param>
@@ -81,19 +81,19 @@ namespace EGM_Projet
         public void FillTorque(string t1, string t2, string t3, string t4, string t5, string t6, string t)
         {
             var newLine = string.Format("T1.append({0})", t1);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("T2.append({0})", t2);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("T3.append({0})", t3);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("T4.append({0})", t4);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("T5.append({0})", t5);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("T6.append({0})", t6);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
             newLine = string.Format("T.append({0})", t);
-            text.AppendLine(newLine);
+            _text.AppendLine(newLine);
         }
 
         /// <summary>
@@ -101,67 +101,67 @@ namespace EGM_Projet
         /// </summary>
         public void Trace()
         {
-            text.AppendLine(" ");
-            text.AppendLine("fig=plt.figure()");
-            text.AppendLine("ax=fig.gca(projection='3d')");
+            _text.AppendLine(" ");
+            _text.AppendLine("fig=plt.figure()");
+            _text.AppendLine("ax=fig.gca(projection='3d')");
 
-            text.AppendLine("ax.plot(X,Y,Z)");
+            _text.AppendLine("ax.plot(X,Y,Z)");
 
-            text.AppendLine("ax.set_xlabel('X')");
-            text.AppendLine("ax.set_ylabel('Y')");
-            text.AppendLine("ax.set_zlabel('Z')");
+            _text.AppendLine("ax.set_xlabel('X')");
+            _text.AppendLine("ax.set_ylabel('Y')");
+            _text.AppendLine("ax.set_zlabel('Z')");
 
-            text.AppendLine("plt.axis('equal')");
-            text.AppendLine("plt.legend(loc='best')");
+            _text.AppendLine("plt.axis('equal')");
+            _text.AppendLine("plt.legend(loc='best')");
 
-            text.AppendLine(" ");
+            _text.AppendLine(" ");
 
-            text.AppendLine("fig2=plt.figure()");
+            _text.AppendLine("fig2=plt.figure()");
 
-            text.AppendLine("plt.plot(TEGM,X,label='x')");
-            text.AppendLine("plt.plot(TEGM,Y,label='y')");
-            text.AppendLine("plt.plot(TEGM,Z,label='z')");
+            _text.AppendLine("plt.plot(TEGM,X,label='x')");
+            _text.AppendLine("plt.plot(TEGM,Y,label='y')");
+            _text.AppendLine("plt.plot(TEGM,Z,label='z')");
 
-            text.AppendLine("plt.xlabel('Temps en ms')");
-            text.AppendLine("plt.ylabel('Axes')");
+            _text.AppendLine("plt.xlabel('Temps en ms')");
+            _text.AppendLine("plt.ylabel('Axes')");
 
-            text.AppendLine("plt.legend(loc='best')");
+            _text.AppendLine("plt.legend(loc='best')");
 
-            text.AppendLine(" ");
+            _text.AppendLine(" ");
 
-            text.AppendLine("fig3=plt.figure()");
+            _text.AppendLine("fig3=plt.figure()");
 
-            text.AppendLine("plt.plot(T,T1,'--',label='Axe 1')");
-            text.AppendLine("plt.plot(T,T2,'--',label='Axe 2')");
-            text.AppendLine("plt.plot(T,T3,'--',label='Axe 3')");
-            text.AppendLine("plt.plot(T,T4,'--',label='Axe 4')");
-            text.AppendLine("plt.plot(T,T5,'--',label='Axe 5')");
-            text.AppendLine("plt.plot(T,T6,'--',label='Axe 6')");
+            _text.AppendLine("plt.plot(T,T1,'--',label='Axe 1')");
+            _text.AppendLine("plt.plot(T,T2,'--',label='Axe 2')");
+            _text.AppendLine("plt.plot(T,T3,'--',label='Axe 3')");
+            _text.AppendLine("plt.plot(T,T4,'--',label='Axe 4')");
+            _text.AppendLine("plt.plot(T,T5,'--',label='Axe 5')");
+            _text.AppendLine("plt.plot(T,T6,'--',label='Axe 6')");
 
-            text.AppendLine("plt.xlabel('Temps en ms')");
-            text.AppendLine("plt.ylabel('Couples en Nm')");
+            _text.AppendLine("plt.xlabel('Temps en ms')");
+            _text.AppendLine("plt.ylabel('Couples en Nm')");
 
-            text.AppendLine("plt.legend(loc='best')");
+            _text.AppendLine("plt.legend(loc='best')");
 
-            text.AppendLine(" ");
+            _text.AppendLine(" ");
 
-            text.AppendLine("if(T==TEGM):");  //Synchronisation condition
+            _text.AppendLine("if(T==TEGM):");  //Synchronisation condition
 
-            text.AppendLine("   fig4=plt.figure()");
-            text.AppendLine("   plt.plot(Y,T1,label='Axe 1')");
+            _text.AppendLine("   fig4=plt.figure()");
+            _text.AppendLine("   plt.plot(Y,T1,label='Axe 1')");
 
-            text.AppendLine("   plt.xlabel('Déplacement en Y en mm')");
-            text.AppendLine("   plt.ylabel('Couple axe 1 en Nm')");
+            _text.AppendLine("   plt.xlabel('Déplacement en Y en mm')");
+            _text.AppendLine("   plt.ylabel('Couple axe 1 en Nm')");
 
-            text.AppendLine("   plt.legend(loc='best')");
+            _text.AppendLine("   plt.legend(loc='best')");
 
-            text.AppendLine("plt.show()");
+            _text.AppendLine("plt.show()");
 
-            File.WriteAllText(filePath, text.ToString());
+            File.WriteAllText(_filePath, _text.ToString());
 
             ProcessStartInfo start = new ProcessStartInfo();
-            string cmd = python;
-            string args = filePath;
+            string cmd = _pythonPath;
+            string args = _filePath;
 
             start.FileName = cmd;
             start.Arguments = args;
@@ -174,11 +174,11 @@ namespace EGM_Projet
         }
 
         /// <summary>
-        /// Clears the StringBuilder text
+        /// Clears the StringBuilder _text
         /// </summary>
         public void Clear()
         {
-            text.Clear();
+            _text.Clear();
         }
     }
 }
