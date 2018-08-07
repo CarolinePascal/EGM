788e767f0eedffebfda42bfdc5062b909c94876e
FormationRobotAdmin <FormationRobotAdmin@FD5820-DX9WHP2.enpc.fr>
2018-08-07 15:40:31 +0200

    Working solution with accurate references

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Net;
-using System.Threading.Tasks;
-using HAL.Communications;
-using HAL.Communications.Protocols;
-using HAL.ENPC.Messaging.Server;
+using HAL.ENPC.Control;
+using HAL.ENPC.Messaging.Server.ABB;
 using HAL.ENPC.Messaging.Server.ATI;
 using HAL.ENPC.Sensoring;
 
@@ -15,67 +12,34 @@ namespace HAL.ENPC.Debug
     {
         static void Main(string[] args)
         {
-            System.Diagnostics.Debug.Listeners.Clear();
-            System.Diagnostics.Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
-            UdpServer.ConfirmationDelay = -1;
 
-            SensorController instance = null;
-            List<ISensor> sensors = new List<ISensor>();
-            Sensor egm = new Sensor(new Identifier("SensorReceiver"), new List<Protocol> { new Electrical() }, new EgmServer(IPAddress.Parse("127.0.0.1"), 6510, 6510));
-            Sensor forceSensor = new Sensor(new Identifier("SensorReceiver"), new List<Protocol> { new Electrical() }, new ForceSensorServer(IPAddress.Parse("192.168.1.1"), 49152, 49152));
-            sensors.AddRange(new[] { /*sensorReceiver,*/ forceSensor });
-            Communication.SensorServer(ref instance, "Name", sensors, forceSensor, false, false, out SensorController controller);
-            Console.WriteLine("Sensor started.");
-            Communication.Run(controller, true);
-            Test(controller);
+            List<RealTimeController> sensors = new List<RealTimeController>();
+            ControlStrategy controlStrategy = null;
+            ControlStrategy.Create("egm", (int)ControlableType.Joint, out controlStrategy);
+
+            EgmServer egmServer = new EgmServer("EgmServer", IPAddress.Parse("127.0.0.1"), 6510, 6510);
+            ForceSensorServer atiServer = new ForceSensorServer("atiServer", IPAddress.Parse("192.168.1.1"), 49152, 49152);
+            Sensor egmSensor = new Sensor(new Identifier("EgmSensor"), null, egmServer);
+            Sensor atiSensor = new Sensor(new Identifier("AtiSensor"), null, atiServer);
+            sensors.Add(egmSensor);
+            sensors.Add(atiSensor);
+            RealTimeController controller = null;
+            RealTimeController.Create(ref controller, "MyController", sensors, controlStrategy, out controller);
+            controller.ConnectAll();
+
+            Console.WriteLine("Enter to staret monitoring.");
             Console.ReadLine();
+
+            controller.Monitor(true, new[] { Messaging.MessageCode.Joints/*, Messaging.MessageCode.Torsor */});
+
+            Console.WriteLine("Enter to terminate");
+            Console.ReadLine();
+            controller.Terminate();
+
+            Console.WriteLine("Enter to exit");
+            Console.ReadLine();
+
         }
 
-        static void Test(SensorController controller)
-        {
-            Sender(controller, 1);
-        }
-
-        static async Task Sender(SensorController controller, int sleepTime)
-        {
-
-            controller.StartSensors();
-            controller.MonitorSensor();
-            if (controller.SenderServer is EgmServer egmServer) egmServer.JointControl(true);
-
-            int i = 1;
-            while (true)
-            {
-                Console.WriteLine($"Sending{i}");
-                Thread.Sleep(sleepTime);
-                //i = -i;
-            }
-        }
-
-
-
-
-
-
-        static void Test(string alias, GenericServer server, int timing)
-        {
-            Sensor sensor = new Sensor(new Identifier(alias), new List<Protocol> { new Electrical() }, server);
-            sensor.SensorServer.Initialize();
-            Sender(sensor, timing);
-        }
-
-
-
-        static async Task Sender(Sensor sensor, int sleepTime)
-        {
-            int i = 0;
-            while (sensor.SensorServer.IsInitialized)
-            {
-                Console.WriteLine($"Sending{i}");
-                await sensor.SensorServer.Enqueue(new StringMessage(MessageCode.String, "Something -------------------------------------------------"), false, null, false);
-                Thread.Sleep(sleepTime);
-                i++;
-            }
-        }
     }
 }
