a14540f2bfb568f6011ea4502d22734cb7cc3542
FormationRobotAdmin <FormationRobotAdmin@FD5820-DX9WHP2.enpc.fr>
2018-08-23 17:33:55 +0200

    New filter class for the average filter

---
+++ Test/Test/AverageFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HAL.ENPC.Filtering;
+using HAL.ENPC.Sensoring.SensorData;
+using System.Collections;
+
+namespace HAL.ENPC.Debug
+{
+    class FilterTorsor : Filter<Torsor>
+    {
+        protected override Torsor FilterMethod()
+        {
+            //do something with that... generate filtered value.
+            Torsor torsor = Torsor.Default;
+            foreach (Torsor t in FilterBuffer)
+            {
+                torsor = torsor.Add(t);
+            }
+            return Divide(torsor, FilterBuffer.Length);
+        }
+        public static Torsor Divide(Torsor torsor, int divider)
+        {
+            return new Torsor(
+                torsor.TX / divider, torsor.TY / divider, torsor.TZ / divider,
+                torsor.RX / divider, torsor.RY / divider, torsor.RZ / divider);
+        }
+    }
+}
