517d78921170c8b5c5838be8ac1e6857312fa79c
FormationRobotAdmin <FormationRobotAdmin@FD5820-DX9WHP2.enpc.fr>
2018-08-23 17:31:54 +0200

    Adding the necessary implemation for filtering purposes

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using HAL.ENPC.Communication;
 using HAL.ENPC.Control;
+using HAL.ENPC.Messaging;
+using HAL.ENPC.Messaging.Server.Generic;
 using HAL.ENPC.Messaging.Server.ABB;
-using HAL.ENPC.Messaging.Server.ATI;
 using HAL.ENPC.Sensoring;
+using HAL.ENPC.Sensoring.SensorData;
+using HAL.ENPC.Messaging.Server.ATI;
+using HAL.ENPC.Filtering;
 
 namespace HAL.ENPC.Debug
 {
@@ -12,34 +19,38 @@ namespace HAL.ENPC.Debug
     {
         static void Main(string[] args)
         {
-
+            // Declaration (sensors)
             List<RealTimeController> sensors = new List<RealTimeController>();
-            ControlStrategy controlStrategy = null;
-            ControlStrategy.Create("egm", (int)ControlableType.Joint, out controlStrategy);
+            //Sensor egmSensor = null;
+            //Sensor.CreateEgm(ref egmSensor, "EgmSensor", "127.0.0.1", 6510, 6510, out egmSensor);
+            //sensors.Add(egmSensor);
 
-            EgmServer egmServer = new EgmServer("EgmServer", IPAddress.Parse("127.0.0.1"), 6510, 6510);
-            ForceSensorServer atiServer = new ForceSensorServer("atiServer", IPAddress.Parse("192.168.1.1"), 49152, 49152);
-            Sensor egmSensor = new Sensor(new Identifier("EgmSensor"), null, egmServer);
-            Sensor atiSensor = new Sensor(new Identifier("AtiSensor"), null, atiServer);
-            sensors.Add(egmSensor);
-            sensors.Add(atiSensor);
+            Filter<Torsor> filter = new FilterTorsor() { FilterSize = 20 };
+            ForceSensorServer forceSensorServer = new ForceSensorServer("ForceSensor", IPAddress.Parse("192.168.1.1"), 49152, 49152, filter: filter);
+            Sensor forceSensor = new Sensor(new Identifier("ForceSensor"), null, forceSensorServer, null);
+            sensors.Add(forceSensor);
+
+            // Declaration (real time controller)
+            ControlBuilder.Create("egm", (int)ControlableType.Joint, out Control.ABB.Egm controlStrategy);
             RealTimeController controller = null;
             RealTimeController.Create(ref controller, "MyController", sensors, controlStrategy, out controller);
-            controller.ConnectAll();
+            controller.BufferMaximumMessageCount = 6000;
 
-            Console.WriteLine("Enter to staret monitoring.");
+            // Monitoring initialization
+            RealTimeController.Monitor(controller, new[] { (int)MessageCode.Torsor, (int)MessageCode.Joints, (int)MessageCode.String, (int)MessageCode.Double }, true, true);
+
+            // Control initialization
+            //RealTimeController.RunControl(controller, true, egmSensor, (int)MessageCode.Joints, true);
+
+            Console.WriteLine("Test run?");
             Console.ReadLine();
 
-            controller.Monitor(true, new[] { Messaging.MessageCode.Joints/*, Messaging.MessageCode.Torsor */});
-
-            Console.WriteLine("Enter to terminate");
+            Console.WriteLine("Edit bug report.");
             Console.ReadLine();
-            controller.Terminate();
+            Analysis.SaveSensorData(controller, true, "C:\\Users\\FormationRobotAdmin\\Documents\\CP019-EGM\\test.csv", out _, out _);
 
-            Console.WriteLine("Enter to exit");
+            Console.WriteLine("Enter to exit.");
             Console.ReadLine();
-
         }
-
     }
 }
