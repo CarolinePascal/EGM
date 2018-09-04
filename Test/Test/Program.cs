6e203bdffcf22f57989c91e2021c7d6ad7efb4cf
FormationRobotAdmin <FormationRobotAdmin@FD5820-DX9WHP2.enpc.fr>
2018-09-04 17:51:22 +0200

    Setting the charging procedure

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -9,6 +9,7 @@ using HAL.ENPC.Messaging.Server.ATI;
 using HAL.ENPC.Sensoring;
 using HAL.ENPC.Sensoring.SensorData;
 using System.Diagnostics;
+using HAL.ENPC.Communication;
 
 namespace HAL.ENPC.Debug
 {
@@ -24,7 +25,7 @@ namespace HAL.ENPC.Debug
             Sensor.CreateEgm(ref egmSensor, "EgmSensor", "127.0.0.1", 6510, 6510, out egmSensor);
             sensors.Add(egmSensor);
 
-            ForceSensorServer forceSensorServer = new ForceSensorServer("ForceSensor", IPAddress.Parse("192.168.1.1"), 49152, 49152, filter: null);
+            ForceSensorServer forceSensorServer = new ForceSensorServer("ForceSensor", IPAddress.Parse("192.168.1.205"), 49152, 49152, filter: null);
             Sensor forceSensor = new Sensor(new Identifier("ForceSensor"), null, forceSensorServer, null);
             sensors.Add(forceSensor);
 
@@ -32,24 +33,27 @@ namespace HAL.ENPC.Debug
             ControlBuilder.Create("egm", (int)ControlableType.Joint, out Control.ABB.Egm controlStrategy);
             RealTimeController controller = null;
             RealTimeController.Create(ref controller, "MyController", sensors, controlStrategy, out controller);
-            controller.BufferMaximumMessageCount = 5;
+            controller.BufferMaximumMessageCount = 6000;
 
             // Monitoring initialization
 
             RealTimeController.Monitor(controller, new[] { (int)MessageCode.Joints, (int)MessageCode.Torsor }, true, true);
 
             // Control initialization
-            RealTimeController.RunControl(controller, true, egmSensor, (int)MessageCode.Joints, true);
+            //RealTimeController.RunControl(controller, true, egmSensor, (int)MessageCode.Joints, true);
 
-            Console.WriteLine("test run ?");
-            Console.ReadLine();
+            //forceSensorServer.Bias();
+            Thread.Sleep(8000);
+            forceSensorServer.Bias();
+            Console.WriteLine("Starting");
 
-            Task.Run(() => RunControlTest(controller));
+            Thread.Sleep(6000);
+
+            Analysis.SaveSensorData(controller, true, "C:\\Users\\FormationRobotAdmin\\Documents\\CP019-EGM\\test.csv", out _, out _);
+
+            //Task.Run(() => RunControlTest(controller));
 
             Console.WriteLine("enter to exit");
-            Console.ReadLine();
-
-            //Analysis.SaveSensorData(controller, true, "C:\\Users\\FormationRobotAdmin\\Documents\\CP019-EGM\\test20m.csv", out _, out_ );
         }
 
         static async Task RunControlTest(RealTimeController controller)
@@ -89,14 +93,14 @@ namespace HAL.ENPC.Debug
 
         public static Joints CreateJointFromTorsor(Torsor torsor, Joints refJoints)
         {
-            Joints targetJoints = new Joints(torsor.TX, torsor.TY, torsor.TZ, torsor.RX, torsor.RY, torsor.RZ);
+            Joints targetJoints = new Joints(2*torsor.TX, 2*torsor.TY, 2*torsor.TZ, 2*torsor.RX, 2*torsor.RY, 2*torsor.RZ);
       
             double j1 = Smooth(refJoints.J1, targetJoints.J1, 0.5);
             double j2 = Smooth(refJoints.J2, targetJoints.J2, 0.5);
             double j3 = Smooth(refJoints.J3, targetJoints.J3, 0.5);
-            double j4 = Smooth(refJoints.J4, targetJoints.J4, 0.5);
-            double j5 = Smooth(refJoints.J5, targetJoints.J5, 0.5);
-            double j6 = Smooth(refJoints.J6, targetJoints.J6, 0.5);
+            double j4 = Smooth(refJoints.J4, targetJoints.J4, 0.75);
+            double j5 = Smooth(refJoints.J5, targetJoints.J5, 0.75);
+            double j6 = Smooth(refJoints.J6, targetJoints.J6, 0.75);
 
             Joints commandJoints = new Joints(j1, j2, j3, j4, j5, j6);
             Console.WriteLine("Commande : " + commandJoints.ToString());
@@ -109,5 +113,20 @@ namespace HAL.ENPC.Debug
             return (start + (target - start) * rate);
         }
 
+        // The normal distribution function.
+        public static double F(double x, double mu, double sigma)
+        {
+            return (Math.Exp(-(x - mu)*(x-mu) / (2*sigma*sigma)) / Math.Sqrt(2*Math.PI*sigma*sigma));
+        }
+
+        public static double[] GaussianPonderation(int n)
+        {
+            double[] array = new double[n];
+            for (int i=0;i<n;i++)
+            {
+                array[i] = F(n - 1 - i,0,1);
+            }
+            return (array);
+        }
     }
 }
