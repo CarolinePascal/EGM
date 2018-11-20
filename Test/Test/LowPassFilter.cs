ec1c8c6e08036845f84f1beb0f17a4165866c139
CarolinePascal <caroline8.pascal@gmail.com>
2018-11-20 10:42:41 +0100

    Adding the Sinc window transformation

--- Test/Test/LowPassFilter.cs
+++ Test/Test/LowPassFilter.cs
@@ -16,20 +16,17 @@ namespace HAL.ENPC.Debug
         /// <param name="bufferSize">Number of coefficients - Size of the window</param>
         /// <param name="cuttingFrequency">Cutting frequency in Hz</param>
         /// <param name="samplingFrequency">Sampling frequency in Hz</param>
-        public LowPassFilter(int bufferSize, double cuttingFrequency, double samplingFrequency) : base(bufferSize)
+        public LowPassFilter(int bufferSize, double cuttingFrequency, double samplingFrequency, double beta) : base(bufferSize)
         {
             if (cuttingFrequency <= 0 || samplingFrequency <= 0)
             {
                 throw new System.Exception("[RIF LPF] Les fréquences doivent être strictement positives");
             }
-            if (bufferSize % 2 == 0)
-            {
-                throw new System.Exception("[RIF LPF] Le nombre de coefficients - bufferSize - doit être impair");
-            }
+            
             else
             {
                 double[] array = new double[bufferSize];
-                double omegaC = 2 * cuttingFrequency / samplingFrequency;
+                double omegaC = 2*cuttingFrequency / samplingFrequency;
 
                 for (int j = 0; j < bufferSize; j++)
                 {
@@ -41,14 +38,32 @@ namespace HAL.ENPC.Debug
                         array[j] = omegaC * Math.Sin(x) / x;
                 }
 
-                coefficients = array;
+                double[] window = new double[bufferSize];
+                for(int i=0;i<bufferSize;i++)
+                {
+                    double x = (2 * i + 1 - bufferSize) * Math.PI / (bufferSize + 1);
+                    if (x > -1.0E-5 && x < 1.0E-5)
+                        window[i] = 1;
+                    else
+                        window[i] = Math.Sin(x) / x;
+                    window[i] = Math.Pow(window[i], beta);
+                }
+
+                double sum = window.Sum();
 
                 for (int i = 0; i < bufferSize; i++)
                 {
-                    Console.WriteLine(coefficients[i]);
+                    window[i] /= sum;
+                    array[i] *= window[i];
                 }
+
+                coefficients = array;
+
+                //for (int i = 0; i < bufferSize; i++)
+                //{
+                //    Console.WriteLine(coefficients[i]);
+                //}
             }
         }
-
     }
 }
