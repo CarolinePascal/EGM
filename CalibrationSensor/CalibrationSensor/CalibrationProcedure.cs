a67b7fbac030abdb9027f22c318deb49c05e4f67
CarolinePascal <caroline8.pascal@gmail.com>
2018-12-13 19:21:08 +0100

    C# solution dedicated to the calibration of the tool/force sensor and the force control of the robot

---
+++ CalibrationSensor/CalibrationSensor/CalibrationProcedure.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HAL.Catalog.Items;
+using HAL.Control;
+using HAL.ENPC.Control;
+using HAL.ENPC.Control.Builder.ABB;
+using HAL.ENPC.Messaging;
+using HAL.ENPC.Sensoring.SensorData;
+using HAL.Objects;
+using HAL.Objects.Mechanisms;
+using HAL.Objects.Parts;
+using HAL.Procedures;
+using HAL.Runtime;
+using HAL.Spatial;
+using HAL.Spatial.Curves;
+using HAL.Spatial.Intersections;
+using HAL.ENPC.Messaging.Server.Generic;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+
+namespace HAL.ENPC.Debug
+{
+    class CalibrationProcedure
+    {
+        /// <summary>
+        /// EGM OnlineController - Needs to be monitoring the MechanismStates of the robot
+        /// </summary>
+        private OnlineController _egm;
+
+        /// <summary>
+        /// Force sensor - Needs to be monitoring the TorsorSensorState of the force sensor
+        /// </summary>
+        private OnlineController _forceSensor;
+
+        /// <summary>
+        /// CalibrationProcedure constructor
+        /// </summary>
+        /// <param name="EGMController">EGM Onclinecontroller - monitoring MechanismStates</param>
+        /// <param name="forceSensorController">Force sensor Onlinecontroller - monitoring TorsorSensorStates</param>
+        public CalibrationProcedure(OnlineController EGMController, OnlineController forceSensorController)
+        {
+            _egm = EGMController;
+            _forceSensor = forceSensorController;
+        }
+
+        /// <summary>
+        /// Launches the calibration procedure
+        /// </summary>
+        /// <returns></returns>
+        public async Task Procedure()
+        {
+            //Calibration positions
+            Joints[] calibrationPositions = new Joints[] { new Joints(0, 0, 0, 0, 0, 0), new Joints(0, 0, 0, 0, 0, 90), new Joints(0, 0, 0, 0, 90, 0) };
+
+            //Initializations 
+            List<Torsor> measures = new List<Torsor>();
+            List<Torsor> buffer = new List<Torsor>();
+
+            Console.WriteLine("Lancement de la calibration - Verifier que tout est en place et appuyer sur entree");
+            Console.ReadLine();
+
+            foreach (Joints joints in calibrationPositions)
+            {
+                //Getting current joints values
+                Joints currentJoints = ((Sensoring.SensorData.Mechanism.EgmMecanismState)_egm.SensorState).Joints;
+
+                //Motion command
+                while (CheckPositon(joints, currentJoints, 0.1) != true)
+                {
+                    EgmBuilder.Joint(_egm, Smoothed(joints, currentJoints, 30).Values);
+                    currentJoints = ((Sensoring.SensorData.Mechanism.EgmMecanismState)_egm.SensorState).Joints;
+                }
+
+                //Stabilization delay
+                await Task.Delay(5000);
+
+                //Getting the forces and torques measurements
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer.Add((Torsor)_forceSensor.SensorState.Value);
+                    await Task.Delay(100);
+                }
+
+                measures.Add(TorsorAverage(buffer));
+            }
+
+
+            Console.WriteLine("Mesures terminées - Calculs en cours");
+
+            double g = 9.81; ;
+            double[] calibrationData = CalibrationAnalysis(measures);
+
+            Console.WriteLine("Calculs terminés - Les valeurs de calibration sont :");
+
+            double p = calibrationData[0] / g;
+
+            Console.WriteLine("Poids : " + p.ToString());
+
+            double d = Math.Atan(calibrationData[2] / calibrationData[1]);
+
+            Console.WriteLine("Dephasage : " + d.ToString());
+
+            double x = calibrationData[6] / calibrationData[0];
+            double y = calibrationData[7] / calibrationData[0];
+            double z = calibrationData[8] / calibrationData[0];
+
+            Console.WriteLine("Position du centre de gravité : [" + x.ToString() + "," + y.ToString() + "," + z.ToString() + "]");
+
+            Console.WriteLine("Offset du capteur : [" + calibrationData[3].ToString() + "," + calibrationData[4].ToString() + "," + calibrationData[5].ToString() + "," + calibrationData[9].ToString() + "," + calibrationData[10].ToString() + "," + calibrationData[11].ToString() + "]");
+        }
+
+        /// <summary>
+        /// Method which computes the average of a list of torsors
+        /// </summary>
+        /// <param name="list">List of Torsor</param>
+        /// <returns></returns>
+        private static Torsor TorsorAverage(List<Torsor> list)
+        {
+            double Fx = 0;
+            double Fy = 0;
+            double Fz = 0;
+            double Tx = 0;
+            double Ty = 0;
+            double Tz = 0;
+
+            foreach (Torsor torsor in list)
+            {
+                Fx += torsor.TX;
+                Fy += torsor.TY;
+                Fz += torsor.TZ;
+                Tx += torsor.RX;
+                Ty += torsor.RY;
+                Tz += torsor.RZ;
+            }
+
+            double size = list.Count();
+
+            Torsor result = new Torsor(Fx / size, Fy / size, Fz / size, Tx / size, Ty / size, Tz / size);
+
+            return (result);
+        }
+
+        /// <summary>
+        /// Method which proceeds the calibation calculus
+        /// </summary>
+        /// <param name="measures">Calibration measures as a List of Torsors</param>
+        /// <returns></returns>
+        private static double[] CalibrationAnalysis(List<Torsor> measures)
+        {
+            //mg\cos(\theta)
+            double[] Tmgc = new double[] { measures[0].TX - measures[2].TX, measures[2].TY - measures[1].TY };
+            //mg\sin(\theta) 
+            double[] Tmgs = new double[] { measures[2].TX - measures[1].TX, measures[2].TY - measures[0].TY };
+
+            for (int i = 0; i < Tmgc.Length; i++)
+            {
+                Console.WriteLine("mgcos" + i.ToString() + " : " + Tmgc[i].ToString());
+                Console.WriteLine("mgsin" + i.ToString() + " : " + Tmgs[i].ToString());
+
+            }
+
+            Console.WriteLine("Continue ?");
+            Console.ReadLine();
+
+            double mgc = Tmgc.Average();
+            double mgs = Tmgs.Average();
+
+            double mg = Math.Sqrt(mgc * mgc + mgs * mgs);
+            double c = mgc / mg;
+            double s = mgs / mg;
+
+            double[] Tfx = new double[] { measures[0].TX - mgc, measures[1].TX + mgs, measures[2].TX };
+            double[] Tfy = new double[] { measures[0].TY + mgs, measures[1].TY + mgc, measures[2].TY };
+            double[] Tfz = new double[] { measures[0].TZ, measures[1].TZ, measures[2].TZ - mg };
+
+            for (int i = 0; i < Tfx.Length; i++)
+            {
+                Console.WriteLine("fx" + i.ToString() + " : " + Tfx[i].ToString());
+                Console.WriteLine("fy" + i.ToString() + " : " + Tfy[i].ToString());
+                Console.WriteLine("fz" + i.ToString() + " : " + Tfz[i].ToString());
+
+            }
+
+            Console.WriteLine("Continue ?");
+            Console.ReadLine();
+
+            double fx = Tfx.Average();
+            double fy = Tfy.Average();
+            double fz = Tfz.Average();
+
+            double[] Txmg = new double[] { c * (measures[2].RZ - measures[1].RZ) + s * (measures[2].RZ - measures[0].RZ), (c * (measures[1].RY - measures[2].RY) + s * (measures[0].RY - measures[2].RY)) / (c + s) };
+            double[] Tymg = new double[] { c * (measures[2].RZ - measures[0].RZ) + s * (measures[1].RZ - measures[2].RZ), (c * (measures[0].RX - measures[2].RX) + s * (measures[1].RX - measures[2].RX)) / (c + s) };
+            double[] Tzmg = new double[] { (measures[0].RX - measures[1].RX) / (s - c), (measures[0].RY - measures[1].RY) / (s + c) };
+
+            for (int i = 0; i < Txmg.Length; i++)
+            {
+                Console.WriteLine("xmg" + i.ToString() + " : " + Txmg[i].ToString());
+                Console.WriteLine("ymg" + i.ToString() + " : " + Tymg[i].ToString());
+                Console.WriteLine("zmg" + i.ToString() + " : " + Tzmg[i].ToString());
+
+            }
+
+            Console.WriteLine("Continue ?");
+            Console.ReadLine();
+
+            double xmg = Txmg.Average();
+            double ymg = Tymg.Average();
+            double zmg = Tzmg.Average();
+
+
+            double[] Tmx = new double[] { measures[0].RX - zmg * s, measures[1].RX - zmg * c, measures[2].RX - ymg };
+            double[] Tmy = new double[] { measures[0].RY - zmg * c, measures[1].RY + zmg * s, measures[2].RY + xmg };
+            double[] Tmz = new double[] { measures[2].RZ, measures[0].RZ + xmg * s + ymg * c, measures[1].RZ + xmg * c - ymg * s };
+
+            for (int i = 0; i < Tmx.Length; i++)
+            {
+                Console.WriteLine("mx" + i.ToString() + " : " + Tmx[i].ToString());
+                Console.WriteLine("my" + i.ToString() + " : " + Tmy[i].ToString());
+                Console.WriteLine("mz" + i.ToString() + " : " + Tmz[i].ToString());
+
+            }
+
+            Console.WriteLine("Continue ?");
+            Console.ReadLine();
+
+            double mx = Tmx.Average();
+            double my = Tmy.Average();
+            double mz = Tmz.Average();
+
+            return (new double[] { mg, c, s, fx, fy, fz, xmg, ymg, zmg, mx, my, mz });
+        }
+
+
+
+        /// <summary>
+        /// Method used to check wether the target is reached or not with respect to a given tolerance
+        /// </summary>
+        /// <param name="orderedJoints">Target to be reached</param>
+        /// <param name="currentJoints">Current position</param>
+        /// <param name="tolerance">Measure tolerance</param>
+        /// <returns></returns>
+        private static bool CheckPositon(Joints orderedJoints, Joints currentJoints, double tolerance)
+        {
+            bool flag = true;
+            for (int i = 0; i < currentJoints.Values.Length; i++)
+            {
+                if (currentJoints.Values[i] < orderedJoints.Values[i] - tolerance || currentJoints.Values[i] > orderedJoints.Values[i] + tolerance)
+                {
+                    flag = false;
+                }
+            }
+            return (flag);
+        }
+
+        /// <summary>
+        /// Method which returns a smoothed joint command according to a maximum joint step
+        /// </summary>
+        /// <param name="orderedJoints">Joints to be reached</param>
+        /// <param name="currentJoints">Current Joints</param>
+        /// <param name="maxStep">Maximum joint step in degrees</param>
+        /// <returns></returns>
+        private static Joints Smoothed(Joints orderedJoints, Joints currentJoints, double maxStep)
+        {
+            Joints joints = new Joints();
+            for (int i = 0; i < orderedJoints.Values.Length; i++)
+            {
+                if (orderedJoints.Values[i] - currentJoints.Values[i] > maxStep)
+                {
+                    joints.Values[i] = currentJoints.Values[i] + maxStep;
+                }
+                else if (orderedJoints.Values[i] - currentJoints.Values[i] < -maxStep)
+                {
+                    joints.Values[i] = currentJoints.Values[i] - maxStep;
+                }
+                else
+                {
+                    joints.Values[i] = orderedJoints.Values[i];
+                }
+            }
+            return (joints);
+        }
+    }
+}
---
+++ CalibrationSensor/CalibrationSensor/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HAL.Catalog.Items;
+using HAL.Control;
+using HAL.ENPC.Control;
+using HAL.ENPC.Control.Builder.ABB;
+using HAL.ENPC.Messaging;
+using HAL.ENPC.Sensoring.SensorData;
+using HAL.Objects;
+using HAL.Objects.Mechanisms;
+using HAL.Objects.Parts;
+using HAL.Procedures;
+using HAL.Runtime;
+using HAL.Spatial;
+using HAL.Spatial.Curves;
+using HAL.Spatial.Intersections;
+using HAL.ENPC.Messaging.Server.Generic;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace HAL.ENPC.Debug
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Measures buffer size
+            int N = 10;
+
+            //Start Session
+            var client = new Client();
+
+            //Load robot - necessary for EGM control and monitoring
+            Mechanism mechanism = LoadMechanism(client).Result;
+
+            //Server emulating the force sensor
+            GenericServer server = new GenericServer("Test", IPAddress.Parse("127.0.0.1"), 5555, 8888);
+            await server.Run();
+
+            SendData(server);
+            await Task.Delay(1000);
+
+            //Force sensor Online controller for the amulated server
+            OnlineController sensor = null;
+            OnlineController.CreateGenericSensor(ref sensor, "Sensor", "127.0.0.1", 8888, 5555, out sensor);
+            sensor.Buffering = true;
+            sensor.Monitor(true, true, MessageCode.TorsorSensorState);
+
+            //Create ATI sensor
+            OnlineController ati = null;
+            OnlineController.CreateAtiSensor(ati, "Ati", "127.0.0.1", 49152, 49152, out ati);
+
+            await ForceMonitor(ati);
+
+            //Create EGM joints control
+            OnlineController controller = InitializeEgmController(mechanism, true);
+            controller.RunSynchronizedControl(MessageCode.MecanismStateEgm, true);
+
+            await Monitor(controller);
+            await Task.Delay(1000);
+
+            //Launching calibration procedure
+            CalibrationProcedure procedure = new CalibrationProcedure(controller, ati);
+            await procedure.Procedure();
+
+            Console.WriteLine("Exit");
+            Console.ReadLine();
+        }
+
+        private static async Task SendData(HAL.ENPC.Messaging.Server.Generic.GenericServer server)
+        {
+            while (true)
+            {
+                await server.SendMessage(new TorsorStateMessage(new HAL.ENPC.Sensoring.TorsorState(new Torsor(0,0,0,0,0,1), false)));
+                await Task.Delay(20);
+            }
+        }
+
+        /// <summary>
+        /// Pure force control command prototype - Constant stiffness control on forces only 
+        /// </summary>
+        /// <param name="forceSensor">Force sensor OnlineController</param>
+        /// <param name="egmControl">EGM OnlineController</param>
+        /// <param name="K">Diagonal items of the stiffness control matrix</param>
+        /// <returns></returns>
+        private static async Task Commande(OnlineController forceSensor, OnlineController egmControl, double[] K)
+        {
+            EgmConstraints settings = new EgmConstraints(ControlStrategy.Debug, 0.1, 0.1, 10);
+            CartesianControl cartControl = new CartesianControl(settings, egmControl);
+            cartControl.Run();
+
+            int n = 0;
+
+            while(n<200)
+            {
+                n++;
+                Torsor torsor = (Torsor)forceSensor.SensorState.Value;
+                Vector3D vector = new Vector3D(K[0]*torsor.TX, K[1]*torsor.TY, K[2]*torsor.TZ);
+
+                UpdateTcpPosition(cartControl, vector, null, null);
+
+                await Task.Delay(500);
+            }
+        }
+
+        /// <summary>
+        /// Method which loads a robot mechanism
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        private static async Task<Mechanism> LoadMechanism(Client client)
+        {
+            Mechanism mechanism = null;
+            Console.WriteLine("type a key word to refine mechanism search : ");
+            string keyWord = Console.ReadLine();
+            foreach (MechanismCatalogItem item in client.Catalogs.Mechanisms.Items.ToList().Where(i => string.IsNullOrEmpty(keyWord) || i.Title.Contains(keyWord)))
+            {
+                if (PromptConfirmation(item.ToString()))
+                {
+                    mechanism = client.Catalogs.Mechanisms.Retrieve(item);
+                    Session.Current.ObjectGraph.AddEdge(new Connection(Session.Current.ObjectGraph.Root as Reference, mechanism.Base) { IsDefaultRootConnection = true });
+                    break;
+                }
+            }
+
+            return mechanism;
+        }
+
+        /// <summary>
+        /// Asynchronous task dedicated to the monitoring of an AIT force sensor
+        /// </summary>
+        /// <param name="atiController">ATI force sensor online controller</param>
+        /// <returns></returns>
+        private static async Task ForceMonitor(OnlineController atiController)
+        {
+            atiController.Buffering = true;
+            atiController.Monitor(true, true, MessageCode.TorsorSensorState);
+        }
+
+        private static async Task Monitor(OnlineController controller)
+        {
+            controller.Buffering = true;
+            controller.Monitor(true, true, MessageCode.MecanismStateEgm);
+            //while(true)
+            //{
+            //    Console.WriteLine(((Sensoring.SensorData.Mechanism.EgmMecanismState)controller.SensorState)?.Joints.ToString());
+            //}
+        }
+
+        /// <summary> Initialize a virtual EGM controller</summary>
+        /// <param name="mechanism"></param>
+        /// <param name="virtualController"></param>
+        /// <returns></returns>
+        private static OnlineController InitializeEgmController(Mechanism mechanism, bool virtualController, params OnlineController[] subcontrollers)
+        {
+            //create egm controller
+            List<IControllableObject> mechanisms = new List<IControllableObject> { mechanism };
+            OnlineController instance = null;
+            List<Procedure> procedures = new List<Procedure>() { new Procedure() };
+            OnlineController.CreateEGM(ref instance, "EgmController", virtualController ? "127.0.0.1" : "192.168.125.1", 6510, 6510, mechanisms, subcontrollers.ToList(), procedures, null, out OnlineController controller);
+            return controller;
+        }
+
+        /// <summary>
+        /// Adds a displacements to the current position - TO BE FIXED
+        /// </summary>
+        /// <param name="control">CartesianControll</param>
+        /// <param name="vector">Vector3D to be added - X,Y,Z</param>
+        /// <param name="mechanism">Robot used as a Mechanism</param>
+        /// <param name="calibrationData">Calibration data computed with the CalibrationProcedure - For the sensor frame</param>
+        private static void UpdateTcpPosition(CartesianControl control, Vector3D vector, Mechanism mechanism, double[] calibrationData)
+        {
+            Console.WriteLine(vector);
+
+            var state = (Sensoring.SensorData.Mechanism.EgmMecanismState)control.Controller.SensorState;
+
+            //Calculating the translation in the robot base frame
+
+            IReadOnlyList<Joint> joints = mechanism.Joints;
+            Motion.JointPositions positions = new Motion.JointPositions(joints, state.Joints.Values);
+            mechanism.Jog(positions, null);
+
+            RotationMatrix endPoint = mechanism.GetActiveEndPointLocation(true).Rotation;
+            RotationMatrix sensor = new RotationMatrix(new double[] { calibrationData[1], -calibrationData[2], 0, calibrationData[2], calibrationData[1], 0, 0, 0, 1 });
+
+            RotationMatrix total = sensor.Multiply(endPoint);
+
+            Vector3D trueVector = (total.Inverse()).Multiply(vector);
+
+            var orientation = state.EndPoint;
+            
+            if (orientation == null)
+            {
+                //return;
+            }
+            QuaternionFrame tcp = new QuaternionFrame(new Vector3D(orientation.Position.Values), new Numerics.Quaternion(orientation.Rotation.Values));
+            vector = trueVector; //with Correction
+            control.ObjectiveFrame = new QuaternionFrame(tcp.Position.Add(in vector), tcp.Rotation);
+            Console.WriteLine(control.ObjectiveFrame.Position);
+            control.MoveTowards(out _, out _);
+        }
+
+        private static bool PromptConfirmation(string confirmText)
+        {
+            Console.Write(confirmText + " [y/n] : ");
+            ConsoleKey response = Console.ReadKey(false).Key;
+            Console.WriteLine();
+            return (response == ConsoleKey.Y);
+        }
+    }
+}
+
